6edad3860b6e796dd31d42700c28a5f140b9ad7a
VazilX <vazilx@gmail.com>
2024-11-30 09:03:11 +0100

    make ready to prodaction timer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,8 @@ import './App.scss';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { Pomodoro } from './components/Pomodoro/Pomodoro';
-// import { useAppDispatch, useAppSelector } from './hooks/redux';
-// import { userSlice } from './store/reducers/example';
 
 export const App: React.FC = () => {
-  // const { count } = useAppSelector(state => state.userReducer);  состояние
-  // const { increment } = userSlice.actions; Экшн
-  // const dispatch = useAppDispatch();
-
-  // dispatch(increment(8));
-
   return (
     <div className="app">
       <Header />
--- src/store/reducers/timer.ts
+++ src/store/reducers/timer.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Stage } from '../../types/Stage';
 
@@ -13,15 +15,10 @@ export interface Timer {
 }
 
 const initialState: Timer = {
-  // seconds: 1500,
-  // focusTime: 1500,
-  // breakTime: 300,
-  // restTime: 900,
-
-  seconds: 2,
-  focusTime: 2,
-  breakTime: 2,
-  restTime: 3,
+  seconds: 1500,
+  focusTime: 1500,
+  breakTime: 300,
+  restTime: 900,
 
   intervalId: undefined,
   isWorking: false,
@@ -34,54 +31,41 @@ export const timerSlice = createSlice({
   initialState,
   reducers: {
     decrement(state: Timer) {
-      // eslint-disable-next-line
       state.seconds--;
     },
     timerSwitch(state: Timer, action: PayloadAction<Stage>) {
-      // eslint-disable-next-line
       state.isWorking = false;
 
       switch (action.payload) {
         case 'break':
-          // eslint-disable-next-line
           state.seconds = state.breakTime;
-          // eslint-disable-next-line
           state.stage = 'break';
           break;
 
         case 'rest':
-          // eslint-disable-next-line
           state.seconds = state.restTime;
-          // eslint-disable-next-line
           state.stage = 'rest';
           break;
 
         default:
-          // eslint-disable-next-line
           state.seconds = state.focusTime;
-          // eslint-disable-next-line
           state.stage = 'focus';
           break;
       }
     },
     start(state: Timer) {
-      // eslint-disable-next-line
       state.isWorking = true;
     },
     pause(state: Timer) {
-      // eslint-disable-next-line
       state.isWorking = false;
     },
     setIntervalId(state: Timer, action: PayloadAction<number | undefined>) {
-      // eslint-disable-next-line
       state.intervalId = action.payload;
     },
     increaseStrick(state: Timer) {
-      // eslint-disable-next-line
       state.strick++;
     },
     refreshStrick(state: Timer) {
-      // eslint-disable-next-line
       state.strick = 0;
     },
   },
